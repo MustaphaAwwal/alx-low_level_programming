ede911a63b26ea2a4a79755d0ceeedc5d9343747
Mustapha Awwal <awwalmustapha41@gmail.com>
2022-04-10 23:11:05 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - reverse print
+ *
+ * Return: success
+ */
+int main(void)
+{
+int i;
+
+	for (i= 'z'; i = 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
