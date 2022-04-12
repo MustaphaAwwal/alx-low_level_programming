5468d07e5aae6389e6591f3db2a892b2fb7f998b
Mustapha Awwal <awwalmustapha41@gmail.com>
2022-04-12 10:28:42 -0700

    hmmm

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+
+}
