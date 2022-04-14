28749e9cbef03668e5c8455dafe511d530cf83ea
Mustapha Awwal <awwalmustapha41@gmail.com>
2022-04-14 11:52:25 -0700

    git

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,6 +8,7 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -24,6 +25,6 @@ int largest_number(int a, int b, int c)
 	{
 	largest = c;
 	}
-	return (largest);
 
+	return (largest);
 }
