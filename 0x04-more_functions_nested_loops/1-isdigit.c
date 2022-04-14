5b2d447a189e736ecca4ff6711341b4201456b1d
Mustapha Awwal <awwalmustapha41@gmail.com>
2022-04-14 14:44:05 -0700

    mint

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check andd confirm
+ * @c: to be checked
+ * Return: always 0
+ */
+int _isdigit(int c)
+{
+	if (c => 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
