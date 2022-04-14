5bdbcf4788945939bd9594f92b6f504ff83573af
Mustapha Awwal <awwalmustapha41@gmail.com>
2022-04-14 13:43:16 -0700

    hfjj

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @c: alphabet to be checked
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
