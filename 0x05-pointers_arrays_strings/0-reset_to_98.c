918bab4875dc1ba4c04e1b21e063ba9583779dad
Mustapha Awwal <awwalmustapha41@gmail.com/>
2022-04-20 06:20:08 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * void reset_to_98 - takes a pointer
- * @*n: to be considered
+ * reset_to_98 - takes a pointer
+ * @n: to be considered
  * Return: 0 always success
  */
 void reset_to_98(int *n)
