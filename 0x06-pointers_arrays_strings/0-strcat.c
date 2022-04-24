efd5934c7732b21e818fb81c17b781b3c6770c8c
Mustapha Awwal <awwalmustapha41@gmail.com/>
2022-04-23 19:44:02 -0700

    array

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+	int result;
+
+	for (i = 0; src[i] != '0'; i++)
+		result[i] = src[i];
+
+
+}
