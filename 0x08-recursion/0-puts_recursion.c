a0da70f131c1a41a1f52561c04c904e5417a2577
Mustapha Awwal <awwalmustapha41@gmail.com/>
2022-04-28 10:53:54 -0700

    jint

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * _puts_recursion - inserts string
+ * @s: to be inserted
+ * Return: always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(s);
+		return _puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
